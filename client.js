df4a8095e0f5fb4c58541682c158e779581c400e
Raymond Mariano <rcm@hawaii.edu>
2015-08-22 19:08:16 -1000

    added request line and header

--- client.js
+++ client.js
@@ -12,8 +12,14 @@ path = argvRegex[3];
 var client = net.connect({port: port, host: host}, function() {
   //'connect' listener
   console.log('connected to server!');
-  client.write('world!\r\n');
+  // client.write('world!\r\n');
+  var date = new Date();
+  var UTCString = date.toUTCString();
 
+  var requestLine = "GET " + path + " HTTP/1.1" + "\n";
+  var requestHeader = "Host: " + host + "\n" + "Connection: Keep-Alive" + "\n" + "Accept: text/html, application/json" + "\n" + "Date: " + UTCString;
+  var request = requestLine + requestHeader;
+  client.write(request);
 
 });
 
